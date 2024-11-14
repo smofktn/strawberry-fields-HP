b856943df2e11fb9885c6183f559f48620fa7a3d
smofktn <meijifj0215@gmail.com>
2024-11-15 00:34:08 +0900

    [add]サイドバー作成

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <v-navigation-drawer>
+    <v-list>
+      <v-list-item title="Home" to="/" :link="true">
+        <!-- <router-link to="/"></router-link> -->
+      </v-list-item>
+      <v-list-item title="about" to="/about" :link="true">
+        <!-- <router-link to="/about"></router-link> -->
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
