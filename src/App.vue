ceec0aab3b193d64ea6716f4c9e4900cb7c7a1f0
smofktn <meijifj0215@gmail.com>
2024-11-15 00:35:10 +0900

    [update]ヘッダーとサイドバーとコンテンツを区分け

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppSidebar from './components/AppSidebar.vue'
+</script>
 
 <template>
-  <div>s</div>
+  <v-app>
+    <header>
+      <v-app-bar>
+        <v-app-bar-title> strawberry fields </v-app-bar-title>
+      </v-app-bar>
+    </header>
+    <body>
+      <v-main>
+        <sidebar>
+          <AppSidebar />
+        </sidebar>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+      <!-- /homeの時 -->
+      <!-- /notificationの時 -->
+      <!-- /contact -->
+      <!-- /obog -->
+    </body>
+  </v-app>
 </template>
